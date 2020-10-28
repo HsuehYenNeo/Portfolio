287b5b1703ccd261d37d3a79e98534d8d589bf79
Hsueh Yen Neo <neo.hsuehyen@gmail.com>
2020-10-28 14:53:53 +0100

    removed text decoration

--- src/App.js
+++ src/App.js
@@ -18,14 +18,16 @@ function App() {
     <div className="App">
       <header className="grid-center">
         <div className="section-container">
-          <div className="headline-container">
-            <div className="heading-font white-font">Neo Hsueh Yen</div>
-            <div className="subheading-font white-font">Frontend Developer</div>
-            <div className="icons-container">
-              <a href="https://www.linkedin.com/in/hsueh-yen-neo/"><div className="icon white-bg"><FaLinkedinIn /></div></a>
-              <a href="https://github.com/Hangrykat"><div className="icon white-bg"><FaGithub /></div></a>
-              <a href="mailto:neo.hsuehyen@gmail.com"><div className="icon white-bg"><FaAt /></div></a>
-              <a href="https://twitter.com/thehangrykat"> <div className="icon white-bg"><FaTwitter /></div></a>
+          <div className="header-container">
+            <div className="headline-container">
+              <div className="heading-font white-font">Neo Hsueh Yen</div>
+              <div className="subheading-font white-font">Frontend Developer</div>
+              <div className="icons-container">
+                <a href="https://www.linkedin.com/in/hsueh-yen-neo/"><div className="icon white-bg"><FaLinkedinIn /></div></a>
+                <a href="https://github.com/Hangrykat"><div className="icon white-bg"><FaGithub /></div></a>
+                <a href="mailto:neo.hsuehyen@gmail.com"><div className="icon white-bg"><FaAt /></div></a>
+                <a href="https://twitter.com/thehangrykat"> <div className="icon white-bg"><FaTwitter /></div></a>
+              </div>
             </div>
           </div>
         </div>
